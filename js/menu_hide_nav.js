6d3412416972ccbbbfa6f76c1adff4936984fc8b
orangejan <39645580+orangejan@users.noreply.github.com>
2018-07-07 17:25:44 +0800

    Add files via upload

--- js/menu_hide_nav.js
+++ js/menu_hide_nav.js
@@ -17,7 +17,7 @@ $(".menu").click(function(e){
         }else{
             $(".back_btn").css("opacity","0.8");
         };
-        // console.log(scrollTop);
+        console.log(scrollTop);
         if(scrollTop>scrollLast&&scrollTop>64){
             $("header").addClass("hide_nav");
         }else{
