f521f6e48949d018ba4b225d990ae9f4699d7767
orangejan <39645580+orangejan@users.noreply.github.com>
2018-08-04 20:05:03 +0800

    php首頁建置

---
+++ foot.php
@@ -0,0 +1,38 @@
+<footer>
+        <div class="wrap">
+            <div class="footer_flex">
+                <div class="footer_icon">
+                    <a href="https://www.facebook.com/art24322684/" target="_blank"><img src="img/facebook.svg" alt="劉鳳珠美術‧黏土才藝教室Facebook粉絲專頁"></a>
+                    <a href="https://line.me/ti/p/rhY4WVTde6" target="_blank"><img src="img/line.svg" alt="劉鳳珠美術‧黏土才藝教室line"></a>
+                    <a class="qrcode" href="https://www.facebook.com/art24322684/" target="_blank"><img src="img/qr.svg" alt="劉鳳珠美術‧黏土才藝教室粉絲專頁qrcode"></a>
+                </div>
+                <ul class="footer_menu">
+                    <li><a href="page_about.html">關於我們</a></li>
+                    <li><a href="">最新消息</a></li>
+                    <li><a href="">一般課程介紹</a></li>
+                    <li><a href="">專業證書課程</a></li>
+                </ul>
+                <ul class="footer_menu">
+                    <li><a href="">作品賞析</a></li>
+                    <li><a href="">藝文活動</a></li>
+                    <li><a href="">聯絡我們</a></li>
+                </ul>
+                <ul class="information">
+                    <li><img src="img/map.svg" alt="劉鳳珠美術‧黏土才藝教室粉絲專頁qrcode"><a href="https://goo.gl/maps/tyjDgtwttVE2" target="_blank">204基隆市安樂區安樂路二段183號</a></li>
+                    <li><img src="img/phone.svg" alt="劉鳳珠美術‧黏土才藝教室粉絲專頁qrcode"><div><a href="tel:0936-270-907">0936-270-907</a><a href="tel:02-2432-2684">02-2432-2684</a></div></li>
+                </ul>
+                <div class="footer_comeon"><img src="img/comeon.svg" alt="劉鳳珠美術‧黏土才藝教室歡迎試上"></a></div>
+            </div>
+            <div class="footer_logo">
+                <a href="#"><img src="img/logo_footer.svg"alt="劉鳳珠美術‧黏土才藝教室logo"></a>
+                <p>Copyright © 2018 劉鳳珠美術・黏土才藝教室 Inc. | All Rights Reserved | Designed by Orangejan.</p>
+            </div>
+        </div>  
+    </footer>
+
+<!--            jq以下             -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/menu_hide_nav.js"></script> 
+    <script></script>
+</body>
+</html>
