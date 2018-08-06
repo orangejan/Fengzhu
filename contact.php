81d7fe646499370a149ff0830482b6623a07c312
orangejan <39645580+orangejan@users.noreply.github.com>
2018-08-06 16:17:00 +0800

    聯繫我們

--- contact.php
+++ contact.php
@@ -3,6 +3,19 @@
 <title>聯絡我們|劉鳳珠美術‧黏土才藝教室</title>
 <!-- 此網頁css寫在下方 -->
 <style>
+    .img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        opacity: .6;
+    }
+    .iframe{
+        display: flex;
+        flex-flow:row wrap;
+        justify-content: center;
+        magin-top:-379px;
+    }
+
 </style>
 <?php include __DIR__ .'/everyone_navbar.php';?>
 <!-- 此網頁麵包穴更改 -->
@@ -11,6 +24,13 @@
             <li><a href="index.php">首頁</a></li>
             <li>聯絡我們</li>
         </ul>
-    </div>
+</div>
  <!-- 此為網頁內容  -->
+<div class="wrap" style="position: relative;"><img class="img" src="img/IMG_0067.JPG" alt=""></div>
+<div class="wrap iframe">
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fart24322684&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="500" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3612.43555308577!2d121.72173035015177!3d25.120961540875943!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x345d4e014a5f648b%3A0x4de54c7f61ddd909!2z5YqJ6bOz54-g576O6KGT6buP5Zyf5omN6Jed54-t!5e0!3m2!1szh-TW!2stw!4v1533461734877" width="500" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+
 <?php include __DIR__ .'/everyone_foot.php';?>
